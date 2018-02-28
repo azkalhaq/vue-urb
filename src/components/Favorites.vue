fbb51c216ba24a07667a6f34f732abb5d1e687de
najihazkalhaq <azkalhaq@gmail.com>
2018-02-28 16:04:53 +0700

    Update Pages

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -1,157 +1,52 @@
 <template>
-    <v-slide-x-transition mode="out-in">
-      <v-layout column align-center>
-        <v-toolbar fixed color="white">
-          <img class="logo" src="https://d1101gxsp27gd6.cloudfront.net/evaluasi-harga/img/logo-urban-gray.png" alt="">
-        </v-toolbar>
-        <div class="property-list">
-          <v-layout row wrap>
-            <v-flex xs12 sm6>
-            <v-card class="card">
-              <v-card-media
-                class="white--text"
-                height="200px"
-                src="https://picture.urbanindo.com/listing/844735957/01/68863eca/dijual-rumah-lux-sayap-pasteur-plus-paviliun/780/780.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline">Top 10 Australian beaches</span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card-media>
-              <v-card-title>
-                <div>
-                  <span class="grey--text">Number 10</span><br>
-                  <span>Whitehaven Beach</span><br>
-                  <span>Whitsunday Island, Whitsunday Islands</span>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">Share</v-btn>
-                <v-btn flat color="orange">Explore</v-btn>
-              </v-card-actions>
-            </v-card>
-            </v-flex>
-            <v-flex xs12 sm6>
-            <v-card class="card">
-              <v-card-media
-                class="white--text"
-                height="200px"
-                src="https://picture.urbanindo.com/listing/844735957/01/68863eca/dijual-rumah-lux-sayap-pasteur-plus-paviliun/780/780.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline">Top 10 Australian beaches</span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card-media>
-              <v-card-title>
-                <div>
-                  <span class="grey--text">Number 10</span><br>
-                  <span>Whitehaven Beach</span><br>
-                  <span>Whitsunday Island, Whitsunday Islands</span>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">Share</v-btn>
-                <v-btn flat color="orange">Explore</v-btn>
-              </v-card-actions>
-            </v-card>
-            </v-flex>
-            <v-flex xs12 sm6>
-            <v-card class="card">
-              <v-card-media
-                class="white--text"
-                height="200px"
-                src="https://picture.urbanindo.com/listing/844735957/01/68863eca/dijual-rumah-lux-sayap-pasteur-plus-paviliun/780/780.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline">Top 10 Australian beaches</span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card-media>
-              <v-card-title>
-                <div>
-                  <span class="grey--text">Number 10</span><br>
-                  <span>Whitehaven Beach</span><br>
-                  <span>Whitsunday Island, Whitsunday Islands</span>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">Share</v-btn>
-                <v-btn flat color="orange">Explore</v-btn>
-              </v-card-actions>
-            </v-card>
-            </v-flex>
-            <v-flex xs12 sm6>
-            <v-card class="card">
-              <v-card-media
-                class="white--text"
-                height="200px"
-                src="https://picture.urbanindo.com/listing/844735957/01/68863eca/dijual-rumah-lux-sayap-pasteur-plus-paviliun/780/780.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline">Top 10 Australian beaches</span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card-media>
-              <v-card-title>
-                <div>
-                  <span class="grey--text">Number 10</span><br>
-                  <span>Whitehaven Beach</span><br>
-                  <span>Whitsunday Island, Whitsunday Islands</span>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">Share</v-btn>
-                <v-btn flat color="orange">Explore</v-btn>
-              </v-card-actions>
-            </v-card>
-            </v-flex>
-            <v-flex xs12 sm6>
-            <v-card class="card">
-              <v-card-media
-                class="white--text"
-                height="200px"
-                src="https://picture.urbanindo.com/listing/844735957/01/68863eca/dijual-rumah-lux-sayap-pasteur-plus-paviliun/780/780.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline">Top 10 Australian beaches</span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card-media>
-              <v-card-title>
-                <div>
-                  <span class="grey--text">Number 10</span><br>
-                  <span>Whitehaven Beach</span><br>
-                  <span>Whitsunday Island, Whitsunday Islands</span>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">Share</v-btn>
-                <v-btn flat color="orange">Explore</v-btn>
-              </v-card-actions>
-            </v-card>
-            </v-flex>
-          </v-layout>
-        </div>
-      </v-layout>
-    </v-slide-x-transition>
+	<v-slide-y-transition mode="out-in">
+	  <v-layout column align-center>
+		<v-toolbar fixed extended color="white">
+		  <img class="logo" src="https://d1101gxsp27gd6.cloudfront.net/evaluasi-harga/img/logo-urban-gray.png" alt="">
+      <v-tabs
+        v-model="tabs"
+        fixed-tabs
+        color="transparent"
+        slot="extension"
+      >
+        <v-tabs-slider></v-tabs-slider>
+        <v-tab-item href="#mobile-tabs-5-1" class="primary--text">
+          <v-icon>phone</v-icon>
+        </v-tab-item>
+        <v-tab-item href="#mobile-tabs-5-2" class="primary--text">
+          <v-icon>favorite</v-icon>
+        </v-tab-item>
+        <v-tab-item href="#mobile-tabs-5-3" class="primary--text">
+          <v-icon>account_box</v-icon>
+        </v-tab-item>
+      </v-tabs>
+		</v-toolbar>
+		<div class="property-list">
+		  <v-tabs-items v-model="tabs" class="white elevation-1">
+      <v-tab-item
+        v-for="i in 3"
+        :key="i"
+        :id="'mobile-tabs-5-' + i"
+      >
+        <v-card>
+          <v-card-text>{{ text }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+		</div>
+	  </v-layout>
+	</v-slide-y-transition>
 </template>
-
+<script>
+  export default {
+    data () {
+      return {
+        tabs: null,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      }
+    }
+  }
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
@@ -179,11 +74,6 @@ a {
   margin-bottom: 20px !important;
   margin-top: 20px !important;
   /* border: 1px solid grey; */
-}
-.property-list{
-  margin-top: 60px;
-  margin-bottom: 60px;
-  width: 100%;
 }
 .card{
   margin: 10px;
